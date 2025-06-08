b6ea0727f1ccfad3406b61e3a3e9b94b5c1b67e5
redeyealone989 <manisharya00000000@gmail.com>
2025-06-08 13:35:54 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,15 @@ load_dotenv()
 
 API_ID = 25667269
 API_HASH = "9941534ef2a1b5466e157baf26f29db2"
-BOT_TOKEN = "7017405982:AAF2XZv-lGxm5nBjGfXxPQFQ6XzP74gueLo"
-MONGO_URL = "mongodb+srv://rohit6205881743:rohit6205881743@cluster0.soqtewz.mongodb.net/"
-MUST_JOIN = "PrimaxPortal"
+BOT_TOKEN = "8119461812:AAEOS5_saEoMILaQyYCNtta-UlwoGAehqh8"
+MONGO_URL = "mongodb+srv://aryatushar409:Wxu2b5IhUxl5cyUr@cluster0.apkfr.mongodb.net/"
+MUST_JOIN = "PlusOtpUpdates"
 SUDO_USERS = [7047049923]
-UPI_ID = "9568536542@fam"
-USDT = "0x57455871d9d48Ab09A67C82C41B95204e6B376c7"
+UPI_ID = "aryasonali@fam"
+USDT = "TLzyRUJcdqaHgws74grysZwnjEGSYCbyci"
+USDT_TRC20 = "TLzyRUJcdqaHgws74grysZwnjEGSYCbyci"
+USDT_BEP20 = "0x50a243b134e8a9a4b4f7c49666a3cacf900823b5"
+BINANCE = 1040058503
 OWNER_ID = 7047049923
+LOG_ID = -1002506464807
 otp_dict = {}
